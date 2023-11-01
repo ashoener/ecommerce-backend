77fae212fd105f23de32654e4675c9364bfb2235
Andrew Shoener <a.b.shoener@gmail.com>
2023-11-01 15:40:06 -0500

    Implement create tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,8 +29,14 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const tag = await Tag.create(req.body);
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
