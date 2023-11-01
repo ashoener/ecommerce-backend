b77c3cd23846f80cbdbe138f0976f836e0d0f967
Andrew Shoener <a.b.shoener@gmail.com>
2023-11-01 15:41:14 -0500

    Implement delete tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -53,8 +53,18 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
