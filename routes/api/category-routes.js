54b5a682fadd223f245a3c481834952ac739b97c
Andrew Shoener <a.b.shoener@gmail.com>
2023-11-01 15:35:17 -0500

    Implement get one category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,6 +19,14 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [Product],
+    });
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post("/", (req, res) => {
